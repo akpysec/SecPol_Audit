377c366972d46e657b0f33558b05b47caa4c553c
akpysec <andriykolihanov@gmail.com>
2022-01-17 15:46:31 +0200

    multiple files scan feature added

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import os
 from engine.cli_args import args
 import engine.secpol_reader as checks
 import colored
@@ -12,46 +13,72 @@ BOLD_TURQUOISE = colored.fg("dark_turquoise") + colored.attr("bold")
 BOLD_ORANGE = colored.fg("dark_orange_3a") + colored.attr("bold")
 BOLD_YELLOW = colored.fg("yellow_3b") + colored.attr("bold")
 
-one = checks.registry_values_check(
-    sec_pol_object=checks.sec_pol_reader(
-        sec_pol_file=args.input_path)
-)
+writer = pd.ExcelWriter(args.out_path + "Parsed_SecPol.xlsx", engine='xlsxwriter')
 
-two = checks.system_access_check(
-    sec_pol_object=checks.sec_pol_reader(
-        sec_pol_file=args.input_path),
-    dictionary_supplied=args.brute_dict
-)
+path_to_files = []
 
-three = checks.easy_check(
-    sec_pol_object=checks.sec_pol_reader(
-        sec_pol_file=args.input_path)
-)
+if len(args.input_path) >= 2:
+    path_to_files.append(args.input_path)
 
-total = one + two + three
+    # Iterate over .csv files in a path
+    files = [x for x in os.listdir(path=path_to_files[0]) if x.lower().endswith(".inf")]
+    for file in files:
 
-if args.out_path:
-    dataframe = pd.DataFrame(total)
-    dataframe.columns = ["Status", "Name", "Configuration"]
-    dataframe.sort_values(by="Status", inplace=True)
-    dataframe.to_csv(args.out_path, index=False)
+        one = checks.registry_values_check(
+            sec_pol_object=checks.sec_pol_reader(
+                sec_pol_file=path_to_files[0] + file)
+        )
 
-for t in sorted(total):
-    if len(t) == 3:
-        if t[0] == checks.check_status[0]:
-            print(stylize(t[0], BOLD_GREEN), t[1], stylize(t[2], BOLD_TURQUOISE))
-        elif t[0] == checks.check_status[1]:
-            print(stylize(t[0], BOLD_RED), t[1], stylize(t[2], BOLD_TURQUOISE))
-        elif t[0] == checks.check_status[3]:
-            print(stylize(t[0], BOLD_BLUE), t[1], stylize(t[2], BOLD_TURQUOISE))
-        elif t[0] == checks.check_status[2]:
-            print(stylize(t[0], BOLD_ORANGE), t[1], stylize(t[2], BOLD_TURQUOISE))
-    elif len(t) == 4:
-        if t[0] == checks.check_status[0]:
-            print(stylize(t[0], BOLD_GREEN), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
-        elif t[0] == checks.check_status[1]:
-            print(stylize(t[0], BOLD_RED), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
-        elif t[0] == checks.check_status[3]:
-            print(stylize(t[0], BOLD_BLUE), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
-        elif t[0] == checks.check_status[2]:
-            print(stylize(t[0], BOLD_ORANGE), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+        two = checks.system_access_check(
+            sec_pol_object=checks.sec_pol_reader(
+                sec_pol_file=path_to_files[0] + file),
+            dictionary_supplied=args.brute_dict
+        )
+
+        three = checks.easy_check(
+            sec_pol_object=checks.sec_pol_reader(
+                sec_pol_file=path_to_files[0] + file)
+        )
+
+        total = one + two + three
+
+        if args.out_path:
+            dataframe = pd.DataFrame(total)
+            if len(dataframe.columns) == 3:
+                dataframe.columns = ["Status", "Name", "Configuration"]
+                dataframe.sort_values(by="Status", inplace=True)
+                dataframe.to_excel(writer, sheet_name=file, startrow=0, index=False)
+            elif len(dataframe.columns) == 4:
+                dataframe.columns = ["Status", "Name", "Configuration", "Comments"]
+                dataframe.sort_values(by="Status", inplace=True)
+                dataframe.to_excel(writer, sheet_name=file, startrow=0, index=False)
+            else:
+                print("Column count miss match")
+
+        print()
+        print("-" * 150)
+        print(f"Checking file: {path_to_files[0] + file}")
+        print("-" * 150)
+        print()
+
+        for t in sorted(total):
+            if len(t) == 3:
+                if t[0] == checks.check_status[0]:
+                    print(stylize(t[0], BOLD_GREEN), t[1], stylize(t[2], BOLD_TURQUOISE))
+                elif t[0] == checks.check_status[1]:
+                    print(stylize(t[0], BOLD_RED), t[1], stylize(t[2], BOLD_TURQUOISE))
+                elif t[0] == checks.check_status[3]:
+                    print(stylize(t[0], BOLD_BLUE), t[1], stylize(t[2], BOLD_TURQUOISE))
+                elif t[0] == checks.check_status[2]:
+                    print(stylize(t[0], BOLD_ORANGE), t[1], stylize(t[2], BOLD_TURQUOISE))
+            elif len(t) == 4:
+                if t[0] == checks.check_status[0]:
+                    print(stylize(t[0], BOLD_GREEN), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+                elif t[0] == checks.check_status[1]:
+                    print(stylize(t[0], BOLD_RED), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+                elif t[0] == checks.check_status[3]:
+                    print(stylize(t[0], BOLD_BLUE), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+                elif t[0] == checks.check_status[2]:
+                    print(stylize(t[0], BOLD_ORANGE), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+
+writer.save()
--- engine/cli_args.py
+++ engine/cli_args.py
@@ -3,19 +3,19 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Creating flags
-parser.add_argument('-I', '--input-file',
+parser.add_argument('-I', '--input-path',
                     required=True,
                     default=None,
                     dest='input_path',
-                    help='Provide path to exported "secpol.inf" file.',
+                    help='Provide path to exported "secpol.inf" file(s).',
                     type=str
                     )
 
-parser.add_argument('-O', '--output-file',
+parser.add_argument('-O', '--output-path',
                     required=False,
                     default=None,
                     dest='out_path',
-                    help='Provide output path and file name with .CSV extension',
+                    help='Provide output path for Parsed_Secpol.xlsx file',
                     type=str
                     )
 
