9ac52469e23501b9116a3b35ac61d649a687872d
akpysec <andriykolihanov@gmail.com>
2022-01-17 15:56:12 +0200

    minor change, check for empty path

--- __main__.py
+++ __main__.py
@@ -22,63 +22,68 @@ if len(args.input_path) >= 2:
 
     # Iterate over .csv files in a path
     files = [x for x in os.listdir(path=path_to_files[0]) if x.lower().endswith(".inf")]
-    for file in files:
+    if len(files) > 0:
+        for file in files:
 
-        one = checks.registry_values_check(
-            sec_pol_object=checks.sec_pol_reader(
-                sec_pol_file=path_to_files[0] + file)
-        )
+            one = checks.registry_values_check(
+                sec_pol_object=checks.sec_pol_reader(
+                    sec_pol_file=path_to_files[0] + file)
+            )
 
-        two = checks.system_access_check(
-            sec_pol_object=checks.sec_pol_reader(
-                sec_pol_file=path_to_files[0] + file),
-            dictionary_supplied=args.brute_dict
-        )
+            two = checks.system_access_check(
+                sec_pol_object=checks.sec_pol_reader(
+                    sec_pol_file=path_to_files[0] + file),
+                dictionary_supplied=args.brute_dict
+            )
 
-        three = checks.easy_check(
-            sec_pol_object=checks.sec_pol_reader(
-                sec_pol_file=path_to_files[0] + file)
-        )
+            three = checks.easy_check(
+                sec_pol_object=checks.sec_pol_reader(
+                    sec_pol_file=path_to_files[0] + file)
+            )
 
-        total = one + two + three
+            total = one + two + three
 
-        if args.out_path:
-            dataframe = pd.DataFrame(total)
-            if len(dataframe.columns) == 3:
-                dataframe.columns = ["Status", "Name", "Configuration"]
-                dataframe.sort_values(by="Status", inplace=True)
-                dataframe.to_excel(writer, sheet_name=file, startrow=0, index=False)
-            elif len(dataframe.columns) == 4:
-                dataframe.columns = ["Status", "Name", "Configuration", "Comments"]
-                dataframe.sort_values(by="Status", inplace=True)
-                dataframe.to_excel(writer, sheet_name=file, startrow=0, index=False)
-            else:
-                print("Column count miss match")
+            if args.out_path:
+                dataframe = pd.DataFrame(total)
+                if len(dataframe.columns) == 3:
+                    dataframe.columns = ["Status", "Name", "Configuration"]
+                    dataframe.sort_values(by="Status", inplace=True)
+                    dataframe.to_excel(writer, sheet_name=file, startrow=0, index=False)
+                elif len(dataframe.columns) == 4:
+                    dataframe.columns = ["Status", "Name", "Configuration", "Comments"]
+                    dataframe.sort_values(by="Status", inplace=True)
+                    dataframe.to_excel(writer, sheet_name=file, startrow=0, index=False)
+                else:
+                    print("Column count miss match")
 
-        print()
-        print("-" * 150)
-        print(f"Checking file: {path_to_files[0] + file}")
-        print("-" * 150)
-        print()
+            print()
+            print("-" * 150)
+            print(f"Checking file: {path_to_files[0] + file}")
+            print("-" * 150)
+            print()
 
-        for t in sorted(total):
-            if len(t) == 3:
-                if t[0] == checks.check_status[0]:
-                    print(stylize(t[0], BOLD_GREEN), t[1], stylize(t[2], BOLD_TURQUOISE))
-                elif t[0] == checks.check_status[1]:
-                    print(stylize(t[0], BOLD_RED), t[1], stylize(t[2], BOLD_TURQUOISE))
-                elif t[0] == checks.check_status[3]:
-                    print(stylize(t[0], BOLD_BLUE), t[1], stylize(t[2], BOLD_TURQUOISE))
-                elif t[0] == checks.check_status[2]:
-                    print(stylize(t[0], BOLD_ORANGE), t[1], stylize(t[2], BOLD_TURQUOISE))
-            elif len(t) == 4:
-                if t[0] == checks.check_status[0]:
-                    print(stylize(t[0], BOLD_GREEN), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
-                elif t[0] == checks.check_status[1]:
-                    print(stylize(t[0], BOLD_RED), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
-                elif t[0] == checks.check_status[3]:
-                    print(stylize(t[0], BOLD_BLUE), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
-                elif t[0] == checks.check_status[2]:
-                    print(stylize(t[0], BOLD_ORANGE), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+            for t in sorted(total):
+                if len(t) == 3:
+                    if t[0] == checks.check_status[0]:
+                        print(stylize(t[0], BOLD_GREEN), t[1], stylize(t[2], BOLD_TURQUOISE))
+                    elif t[0] == checks.check_status[1]:
+                        print(stylize(t[0], BOLD_RED), t[1], stylize(t[2], BOLD_TURQUOISE))
+                    elif t[0] == checks.check_status[3]:
+                        print(stylize(t[0], BOLD_BLUE), t[1], stylize(t[2], BOLD_TURQUOISE))
+                    elif t[0] == checks.check_status[2]:
+                        print(stylize(t[0], BOLD_ORANGE), t[1], stylize(t[2], BOLD_TURQUOISE))
+                elif len(t) == 4:
+                    if t[0] == checks.check_status[0]:
+                        print(stylize(t[0], BOLD_GREEN), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+                    elif t[0] == checks.check_status[1]:
+                        print(stylize(t[0], BOLD_RED), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+                    elif t[0] == checks.check_status[3]:
+                        print(stylize(t[0], BOLD_BLUE), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+                    elif t[0] == checks.check_status[2]:
+                        print(stylize(t[0], BOLD_ORANGE), t[1], stylize(t[2], BOLD_TURQUOISE), stylize(t[3], BOLD_ORANGE))
+    elif len(files) == 0:
+        print(f"\nNo files with extension .inf were found in the specified path:\n\n- {args.input_path}\n\n"
+              f"Specify different path & try again.\n")
+
 
 writer.save()
